25f2856f9907fb2b67da049496b72e129db0a7db
ChristopheC83 <christophe.chiappetta@gmail.com>
2023-08-06 20:41:08 +0200

    début du projet

---
+++ src/Components/Body.jsx
@@ -0,0 +1,12 @@
+
+
+function Body() {
+  return (
+    <div className="bodyContent">
+        <h2>Corps de la SPA</h2>
+      
+    </div>
+  )
+}
+
+export default Body
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+
+
+function Header() {
+  return (
+    <div className="header">
+        <h2>To Do List React 2023</h2>
+      
+    </div>
+  )
+}
+
+export default Header
